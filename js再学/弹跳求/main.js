8f2e0a5a8224a7c77b6e537e5ba30e04ca1ff5eb
hl770969104 <770969104@qq.com>
2018-11-28 19:27:31 +0800

    js实验

---
+++ js再学/弹跳求/main.js
@@ -0,0 +1,60 @@
+var canvas = document.querySelector('canvas');
+
+var ctx = canvas.getContext('2d');
+
+
+
+var width = canvas.width = window.innerWidth;
+
+var height = canvas.height = window.innerHeight;
+
+
+
+// function to generate random number
+
+
+
+function random(min,max) {
+
+  var num = Math.floor(Math.random()*(max-min)) + min;
+
+  return num;
+
+}
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
